2e95130eec1ed9db9e9311280ba48d2a33244d33
Shobika06 <shobikak.24cse@kongu.edu>
2026-02-15 16:34:04 +0530

    Time: 0 ms (100%), Space: 14.6 MB (70.14%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        int total = 1 << n;   // 2^n
+        
+        for (int i = 0; i < total; i++) {
+            result.push_back(i ^ (i >> 1));
+        }
+        
+        return result;
+    }
+};
+
